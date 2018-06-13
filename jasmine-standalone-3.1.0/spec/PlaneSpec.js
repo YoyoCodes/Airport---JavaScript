b4c585534215041d8922b32dd6b0e89bfa5ea1fb
Laura Petre <laura.d.petre@gmail.com>
2018-06-13 19:45:28 +0100

    Final user story complete - green tests

--- jasmine-standalone-3.1.0/spec/PlaneSpec.js
+++ jasmine-standalone-3.1.0/spec/PlaneSpec.js
@@ -1,33 +1,19 @@
-'use strict'
+'use strict';
+
 describe('Plane',function(){
   var plane;
   var airport;
-
-  describe('when its not stormy',function(){
-    beforeEach(function(){
-      plane = new Plane();
-      airport = jasmine.createSpyObj('airport',['clearForLanding','clearForTakeOff']);
-    });
-
-    it('can land at an airport', function(){
-      plane.land(airport);
-      expect(airport.clearForLanding).toHaveBeenCalledWith(plane);
-    });
-    it('can takeoff from an airport', function(){
-      plane.land(airport);
-      plane.takeoff();
-      expect(airport.clearForTakeOff).toHaveBeenCalled();
-    });
+  beforeEach(function(){
+    plane = new Plane();
+    airport = jasmine.createSpyObj('airport',['clearForLanding','clearForTakeOff']);
   });
-
-  // describe('when its not stormy',function(){
-  //   beforeEach(function(){
-  //     plane = new Plane();
-  //   });
-  //   it('cannot take off from an airport when its stormy',function(){
-  //     plane.land(airport);
-  //     plane.takeoff();
-  //     expect(airport.clearForTakeOff).toHaveBeenCalled();
-  //   });
-  // });
+  it('can land at an airport', function(){
+    plane.land(airport);
+    expect(airport.clearForLanding).toHaveBeenCalledWith(plane);
+  });
+  it('can takeoff from an airport', function(){
+    plane.land(airport);
+    plane.takeoff();
+    expect(airport.clearForTakeOff).toHaveBeenCalled();
+  });
 });
